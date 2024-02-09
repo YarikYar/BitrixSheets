bf042b379bcf34dfd897749a305314ed190d1eeb
Yaroslav <zaruchevskiy@stm32f0.ru>
2024-02-09 10:38:27 +0300

    minor

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,8 @@ COPY go.mod go.sum /bitrix/
 RUN go mod download
 COPY main.go cred.json token.json /bitrix/
 EXPOSE 2727
-RUN go run .
+RUN apt update
+RUN apt install sqlite3
+RUN go build .
+
+CMD ["./awesomeProject"]
--- main.go
+++ main.go
@@ -60,27 +60,9 @@ func getClient(config *oauth2.Config) *http.Client {
 	tokFile := "token.json"
 	tok, err := tokenFromFile(tokFile)
 	if err != nil {
-		tok = getTokenFromWeb(config)
-		saveToken(tokFile, tok)
+		log.Fatalln("NO TOKEN")
 	}
 	return config.Client(context.Background(), tok)
-}
-
-func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
-	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
-	fmt.Printf("Go to the following link in your browser then type the "+
-		"authorization code: \n%v\n", authURL)
-
-	var authCode string
-	if _, err := fmt.Scan(&authCode); err != nil {
-		log.Fatalf("Unable to read authorization code: %v", err)
-	}
-
-	tok, err := config.Exchange(context.TODO(), authCode)
-	if err != nil {
-		log.Fatalf("Unable to retrieve token from web: %v", err)
-	}
-	return tok
 }
 
 func tokenFromFile(file string) (*oauth2.Token, error) {
@@ -92,16 +74,6 @@ func tokenFromFile(file string) (*oauth2.Token, error) {
 	tok := &oauth2.Token{}
 	err = json.NewDecoder(f).Decode(tok)
 	return tok, err
-}
-
-func saveToken(path string, token *oauth2.Token) {
-	fmt.Printf("Saving credential file to: %s\n", path)
-	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
-	if err != nil {
-		log.Fatalf("Unable to cache oauth token: %v", err)
-	}
-	defer f.Close()
-	json.NewEncoder(f).Encode(token)
 }
 
 func main() {
